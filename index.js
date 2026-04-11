0aa09c42ec35eb4b75ba45b947b879a97e6414a1
Mahfuzur Rahman <mahfuzurrahman0012@gmail.com>
2026-04-11 22:28:03 +0600

    update

--- index.js
+++ index.js
@@ -100,6 +100,19 @@ async function run() {
       res.send(result);
     });
 
+    app.patch('/users/:id', async (req, res)=>{
+      const id = req.params.id;
+      const roleInfo = req.body;
+      const query = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set:{
+          role: roleInfo.role
+        }
+      }
+      const result = await usersCollection.updateOne(query, updateDoc);
+      res.send (result)
+    })
+
     // =============================================================================
     // parcel api
     app.get("/parcels", async (req, res) => {
