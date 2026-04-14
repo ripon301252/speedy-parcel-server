942d2c5ab59b02a92f8ee72cc7a8b186f87bd28d
Mahfuzur Rahman <mahfuzurrahman0012@gmail.com>
2026-04-15 00:47:06 +0600

    update

--- index.js
+++ index.js
@@ -187,6 +187,24 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/parcels/rider', async (req, res)=>{
+      const {riderEmail, deliveryStatus} = req.query;
+      const query = {};
+
+      if(riderEmail){
+        query.riderEmail = riderEmail
+      }
+
+      if(deliveryStatus){
+        query.deliveryStatus = {$in: ['driver_assigned', "rider_accepted"]}
+      }
+      
+      
+      const cursor = parcelsCollection.find(query)
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
     app.get("/parcels/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -201,6 +219,7 @@ async function run() {
       res.send(result);
     });
 
+    // TODO: rename this to be specific like /parcels/:id/assign
     app.patch("/parcels/:id", async (req, res) => {
       const { riderId, riderName, riderEmail, parcelId } = req.body;
       const id = req.params.id;
@@ -228,6 +247,18 @@ async function run() {
 
       res.send(riderResult);
     });
+
+    app.patch('/parcels/:id/deliveryStatus', async(req, res)=>{
+      const {deliveryStatus} = req.body;
+      const query = {_id: new ObjectId(req.params.id)};
+      const updatedDoc = {
+        $set: {
+          deliveryStatus: deliveryStatus
+        }
+      }
+      const result = await parcelsCollection.updateOne(query, updatedDoc)
+      res.send(result)
+    })
 
     app.delete("/parcels/:id", async (req, res) => {
       const id = req.params.id;
